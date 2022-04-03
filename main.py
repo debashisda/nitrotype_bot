025fd19df624b3c262ec08a24e28be70f91471ae
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-03 14:10:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import time
+import os
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+import time
 
 username = "nitrotype_bot_test3"
 password = "http$3A%2F"
-typing_speed = 1/7 #almost around 75 - 80 WPM
+typing_speed = 1/(7.6) #almost around 75 - 80 WPM
 chromedriver_path = r'C:/chromedriver.exe'
 
+def clear_window():
+    clear = lambda:os.system('cls')
+    clear()
+    
 def prepare_browser():
     global driver
     options = Options()
@@ -18,7 +23,7 @@ def prepare_browser():
     driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
     
 def fetch_paragraph():
-    driver.execute_script("a=[];e=document.getElementsByClassName('dash-letter');for(let i=0;i<(document.getElementsByClassName('dash-letter').length);i++){a[i]=e[i].innerHTML;}")
+    driver.execute_script("a=[];e=document.getElementsByClassName('dash-letter');l=document.getElementsByClassName('dash-letter').length;for(let i=0;i<l;i++){a[i]=e[i].innerHTML;}")
     paragraph = driver.execute_script("return a;")
     paragraph = paragraph[:-1]
     return paragraph
@@ -47,6 +52,7 @@ def race_with_players():
     time.sleep(5)
     if driver.find_element_by_xpath("//*[@id='raceContainer']/div[1]/div[2]/div[4]/div/div[2]/button").is_displayed() == True:
         print("Race Finished...!")
+        time.sleep(2)
 
 def check_race_invites():
     count = driver.execute_script("return document.getElementsByClassName('growl').length;")
@@ -56,31 +62,46 @@ def check_race_invites():
             link = driver.execute_script("return document.getElementsByTagName('a')[0].getAttribute('href');")
             return link
         elif ch == 'N' or ch == 'n':
-            driver.find_element_by_xpath("//*[@id='root']/div[1]/div/button").click()
+            return -1
+            #driver.find_element_by_xpath("//*[@id='root']/div[1]/div/button").click()
     else:
-        return 0;
+        return 0
 
+def race_with_friend():    
+    race_link = check_race_invites()
+    if race_link != 0 or race_link !=-1:
+        driver.get("https://nitrotype.com" + str(race_link))        
+        race_with_players()
+    elif race_link == -1:
+        print("Ok! You don't want to race with your friend....")
+        time.sleep(2)
+    else:
+        print("No Invitation Found...")
+        time.sleep(1)    
+
+def logout_and_exit():
+    driver.execute_script("window.localStorage.clear();")
+    driver.close()
+    exit()    
+    
 def main():
     prepare_browser()
     login_to_nitrotype()    
     while(1):
-        ch = int(input("1)Race With Random Players\n2)Check for Invites\n3)Exit\nOption: "))
+        clear_window()        
+        ch = int(input("1)Race With Random Players\n2)Check for Invites\n3)Go to Garage\n4)Logout & Exit\nOption: "))
         if ch == 1: 
             driver.get("https://nitrotype.com/race")
             race_with_players()          
-        elif ch == 2:    
-            race_link = check_race_invites()
-            if race_link != 0:                
-                driver.get("https://nitrotype.com" + str(race_link))
-                driver.find_element_by_xpath("//*[@id='root']/div[1]/div/button").click()
-                race_with_players()
-            else:
-                print("No Invitation Found...")
+        elif ch == 2:            
+            race_with_friend()            
         elif ch == 3:
-            driver.close()
-            exit()
+            driver.get("https://www.nitrotype.com/garage")
+        elif ch == 4:
+            logout_and_exit()            
         else:
-            print("Enter Valid Input !!!")           
+            print("Enter Valid Input !!!")
+            time.sleep(1)
       
 if __name__ == "__main__":
     main()
