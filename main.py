2c9cd90a441bc7f4d5b0257f4d282116aacdb048
Debashis Das <47478624+debashisda@users.noreply.github.com>
2022-04-05 01:08:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import os
 import selenium
+import time
+import os
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
 
-username = "nitrotype_bot_test3"
-password = "http$3A%2F"
-typing_speed = 1/(7.6) #almost around 75 - 80 WPM
-chromedriver_path = r'C:/chromedriver.exe'
+def load_data():
+    global data
+    with open('data.json') as data_file:
+        data = json.load(data_file)
 
 def clear_window():
     clear = lambda:os.system('cls')
@@ -20,63 +21,64 @@ def prepare_browser():
     global driver
     options = Options()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
-    
-def fetch_paragraph():
-    driver.execute_script("a=[];e=document.getElementsByClassName('dash-letter');l=document.getElementsByClassName('dash-letter').length;for(let i=0;i<l;i++){a[i]=e[i].innerHTML;}")
-    paragraph = driver.execute_script("return a;")
-    paragraph = paragraph[:-1]
-    return paragraph
+    driver = webdriver.Chrome(executable_path=data['path'], options=options)
 
 def login_to_nitrotype():
     driver.get("https://www.nitrotype.com/login")
     driver.maximize_window()
-    driver.find_element_by_id("username").send_keys(username)
-    time.sleep(3)
-    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("username").send_keys(data['username'])
+    time.sleep(2)
+    driver.find_element_by_id("password").send_keys(data['password'])
     time.sleep(2)
     driver.find_element_by_xpath("//*[@id='root']/div/div/main/div/section/div[2]/div/div[3]/form/button").click()
-    time.sleep(4)
+    time.sleep(3)
     webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
         
-def race_with_players():  
-    wait = input("Press ENTER when the race starts! ")   
-    paragraph = fetch_paragraph()
+def race_with_players():
+    #race_status = driver.execute_script("return document.getElementsByClassName('bucket-content').length;")
+    #race_status = driver.execute_script("return document.getElementsByClassName('tsxxl mbf tlh-1')[0].innerHTML;")   
+    wait = input("Press ENTER when the race starts! ")
+    driver.execute_script("a=[];e=document.getElementsByClassName('dash-letter');l=document.getElementsByClassName('dash-letter').length;for(let i=0;i<l;i++){a[i]=e[i].innerHTML;}")
+    paragraph = driver.execute_script("return a;")        
+    paragraph = paragraph[:-1]
     for letter in paragraph:
         if letter == '&nbsp;':
-            time.sleep(typing_speed)
+            time.sleep(data['typing_speed'])
             webdriver.ActionChains(driver).send_keys(Keys.SPACE).perform()
         else:
-            time.sleep(typing_speed)
+            time.sleep(data['typing_speed'])
             webdriver.ActionChains(driver).send_keys(letter).perform()
-    time.sleep(5)
-    if driver.find_element_by_xpath("//*[@id='raceContainer']/div[1]/div[2]/div[4]/div/div[2]/button").is_displayed() == True:
-        print("Race Finished...!")
+    time.sleep(5)    
+    if int(driver.execute_script("return Boolean(document.getElementsByClassName('raceResults-titles').length);")) == 1:
+        print("Race Finished...!")        
         time.sleep(2)
+    else:
+        driver.get("https://www.nitrotype.com/garage")        
 
 def check_race_invites():
     count = driver.execute_script("return document.getElementsByClassName('growl').length;")
-    if count >= 1:
+    if count == 1:
         ch = input("Invite Found. Want to join?(Y/N): ")
         if ch == 'Y' or ch == 'y':
             link = driver.execute_script("return document.getElementsByTagName('a')[0].getAttribute('href');")
             return link
         elif ch == 'N' or ch == 'n':
-            return -1
-            #driver.find_element_by_xpath("//*[@id='root']/div[1]/div/button").click()
+            if count >= 1:
+                driver.find_element_by_xpath("//*[@id='root']/div[1]/div/button").click()
+            return 'N'
     else:
         return 0
 
 def race_with_friend():    
     race_link = check_race_invites()
-    if race_link != 0 or race_link !=-1:
+    if race_link != 0 and race_link !='N':
         driver.get("https://nitrotype.com" + str(race_link))        
         race_with_players()
-    elif race_link == -1:
-        print("Ok! You don't want to race with your friend....")
+    elif race_link == 'N':
+        print("Never Mind....")
         time.sleep(2)
     else:
-        print("No Invitation Found...")
+        print("No Invites Found...")
         time.sleep(1)    
 
 def logout_and_exit():
@@ -85,11 +87,13 @@ def logout_and_exit():
     exit()    
     
 def main():
+    load_data()
     prepare_browser()
     login_to_nitrotype()    
     while(1):
-        clear_window()        
-        ch = int(input("1)Race With Random Players\n2)Check for Invites\n3)Go to Garage\n4)Logout & Exit\nOption: "))
+        clear_window()
+        print("1)Race With Random Players\n2)Check for Invites\n3)Go to Garage\n4)Logout & Exit\n")
+        ch = int(input("Option: "))
         if ch == 1: 
             driver.get("https://nitrotype.com/race")
             race_with_players()          
